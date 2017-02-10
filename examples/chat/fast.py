44a88581d934981300da7487b8aff3febad44e39
Fran <ftarulla@gmail.com>
2017-02-10 15:54:33 -0300

    Integración de algoritmo FAST

---
+++ examples/chat/fast.py
@@ -0,0 +1,49 @@
+
+import sys
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
+#
+import json
+
+# args
+#print sys.argv
+
+#https://www.tutorialspoint.com/python/python_command_line_arguments.htm
+filename = sys.argv[1]
+
+img = cv2.imread(filename, 0)
+
+# Initiate FAST object with default values
+fast = cv2.FastFeatureDetector_create()
+# find and draw the keypoints
+keypoints = fast.detect(img, None)
+img2 = cv2.drawKeypoints(img, keypoints, None, color=(255,0,0))
+
+# Print all default params
+print "Threshold: ", fast.getThreshold()
+print "nonmaxSuppression: ", fast.getNonmaxSuppression()
+print "neighborhood: ", fast.getType()
+print "Total Keypoints with nonmaxSuppression: ", len(keypoints)
+cv2.imwrite('fast_true.png', img2)
+
+# Disable nonmaxSuppression
+# fast.setNonmaxSuppression(0)
+# keypoints = fast.detect(img,None)
+# print "Total Keypoints without nonmaxSuppression: ", len(keypoints)
+# img3 = cv2.drawKeypoints(img, keypoints, None, color=(255,0,0))
+# cv2.imwrite('fast_false.png',img3)
+
+kps = []
+for kp in keypoints:
+    kps.append(kp.pt)
+    # point = kp.pt
+    # print point
+    # print point[0]
+    # print point[1]
+    # result += "[%.1f, %.1f], " % ( kp.pt[0], kp.pt[1] )
+# result += "] }'"
+
+sys.stdout.write(json.dumps({'keypoints':kps}))
+# sys.stdout.flush()
--- examples/chat/index.js
+++ examples/chat/index.js
@@ -5,6 +5,33 @@ var server = require('http').createServer(app);
 var io = require('../..')(server);
 var port = process.env.PORT || 3000;
 
+//
+var fs = require('fs');
+const spawn = require("child_process").spawn;
+
+// http://stackoverflow.com/questions/15515549/node-js-writing-a-function-to-return-spawn-stdout-as-a-string
+const runPython = function(args, callback) {
+
+    // http://ourcodeworld.com/articles/read/286/how-to-execute-a-python-script-and-retrieve-output-data-and-errors-in-node-js
+    const uint8arrayToString = function(data){
+      return String.fromCharCode.apply(null, data);
+    };
+
+    // https://nodejs.org/api/child_process.html
+    var proc = spawn('python', args);
+
+    var result = "";
+    proc.stdout.on('data', function(data) {
+      result += uint8arrayToString(data);
+    });
+    proc.stdout.on('close', function(code) {
+      callback(result);
+    });
+}
+
+
+
+//
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
 });
@@ -21,15 +48,42 @@ io.on('connection', function (socket) {
 
   // when the client emits 'new message', this listens and executes
   socket.on('new message', function (data) {
-    // we tell the client to execute 'new message'
-    socket.broadcast.emit('new message', {
-      username: socket.username,
-      message: data
-    });
+    console.log("New Message!");
+    //console.log(data);
+
+    if (data.image) {
+      console.log("SAVING ...");
+      var bitmap = new Buffer(data.image, 'base64');
+      fs.writeFileSync('./bla.png', bitmap);
+      console.log("DONE!");
+
+      //
+      runPython(['./fast.py', 'bla.png'], function(result) {
+        console.log(result);
+        console.log(result.indexOf("{"));
+        var parsed = JSON.parse( result.substring(result.indexOf("{")) );
+        console.log(parsed.keypoints[42]);
+
+        console.log("sending fast response");
+        socket.emit('fast response', {
+          username: socket.username,
+          message: parsed.keypoints[42]
+        });
+
+      });
+    }
+
+    // // we tell the client to execute 'new message'
+    // socket.broadcast.emit('new message', {
+    //   username: socket.username,
+    //   message: data
+    // });
   });
 
   // when the client emits 'add user', this listens and executes
   socket.on('add user', function (username) {
+    console.log("New User!");
+
     if (addedUser) return;
 
     // we store the username in the socket session for this client
@@ -48,6 +102,7 @@ io.on('connection', function (socket) {
 
   // when the client emits 'typing', we broadcast it to others
   socket.on('typing', function () {
+    console.log("User Start Typing!");
     socket.broadcast.emit('typing', {
       username: socket.username
     });
@@ -55,6 +110,7 @@ io.on('connection', function (socket) {
 
   // when the client emits 'stop typing', we broadcast it to others
   socket.on('stop typing', function () {
+    console.log("User Stop Typing!");
     socket.broadcast.emit('stop typing', {
       username: socket.username
     });
@@ -62,6 +118,7 @@ io.on('connection', function (socket) {
 
   // when the user disconnects.. perform this
   socket.on('disconnect', function () {
+    console.log("Disconnect!");
     if (addedUser) {
       --numUsers;
 
